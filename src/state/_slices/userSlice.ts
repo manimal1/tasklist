bc41eff7a507b942d0a8306f38d1c926d10d9655
Jeremiah McCurdy <jeremiah.mccurdy@gmail.com>
2022-04-14 19:10:40 +0200

    add redux store with user state

---
+++ src/state/_slices/userSlice.ts
@@ -0,0 +1,42 @@
+import { createSlice, PayloadAction, createAsyncThunk } from '@reduxjs/toolkit';
+import { RootState } from 'state/store';
+import axios from 'axios';
+
+interface UserSlice {
+  id: string;
+  name: string;
+  image_url: string;
+}
+
+const initialState: UserSlice = {
+  id: '',
+  name: '',
+  image_url: '',
+};
+
+export const getUserData = createAsyncThunk('user/getUserData', async () => {
+  try {
+    const response = await axios.get('http://localhost:3001/user');
+    return response.data;
+  } catch (err) {
+    throw Error('There was an error retrieving the user');
+  }
+});
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(getUserData.fulfilled, (state, { payload: userData }: PayloadAction<UserSlice>) => ({
+      ...state,
+      ...userData,
+    }));
+  },
+});
+
+export const getUserId = (state: RootState) => state.user.id;
+export const getUserName = (state: RootState) => state.user.name;
+export const getUserImage = (state: RootState) => state.user.image_url;
+
+export default userSlice.reducer;
