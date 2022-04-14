f726651ab9f641e89c74557647dd04decbb2aca7
Jeremiah McCurdy <jeremiah.mccurdy@gmail.com>
2022-04-14 22:08:50 +0200

    update test

--- src/+TaskList/containers/TaskListTable/TaskListTable.test.tsx
+++ src/+TaskList/containers/TaskListTable/TaskListTable.test.tsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render, waitFor } from '@testing-library/react';
 import { describe, test, vi } from 'vitest';
 import { Provider } from 'react-redux';
 
@@ -6,25 +6,39 @@ import { store } from 'state/store';
 import { Task } from '+TaskList/types';
 import { TaskListTable } from './TaskListTable';
 
+const refetch = vi.fn();
+vi.mock('+TaskList/services', () => {
+  return { taskAction: () => ({ delete: () => Promise.resolve() }) };
+});
+
 const mockTaskList: Task[] = [
   { id: '1', title: 'title 1', is_complete: false, created_at: new Date().getTime(), user_id: '10' },
   { id: '2', title: 'title 2', is_complete: false, created_at: new Date().getTime(), user_id: '20' },
 ];
 
+const renderedTaskListTable = () =>
+  render(
+    <Provider store={store}>
+      <TaskListTable refetch={refetch} taskList={mockTaskList} isLoading={false} />
+    </Provider>,
+  );
+
 describe('<TaskListTable>', () => {
-  test('renders all tasks titles in task list with action buttons', async () => {
-    const refetch = vi.fn();
-    const { getByText, getByTestId } = render(
-      <Provider store={store}>
-        <TaskListTable refetch={refetch} taskList={mockTaskList} isLoading={false} />
-      </Provider>,
-    );
+  test('renders all tasks titles in task list', async () => {
+    const { getByText } = renderedTaskListTable();
 
     expect(getByText('title 1'));
     expect(getByText('title 2'));
+  });
+
+  test('displays delete button and handles its api query and refetch function', async () => {
+    vi.clearAllMocks();
+    const { getByTestId } = renderedTaskListTable();
+
     expect(getByTestId('delete-1'));
     expect(getByTestId('delete-2'));
-    expect(getByTestId('edit-1'));
-    expect(getByTestId('edit-2'));
+    expect(refetch).toHaveBeenCalledTimes(0);
+    fireEvent.click(getByTestId('delete-1'));
+    waitFor(() => expect(refetch).toHaveBeenCalledTimes(1));
   });
 });
