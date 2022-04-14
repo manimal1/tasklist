99555b1ab734292cf4bb6249c4a5d7ba872ba607
Jeremiah McCurdy <jeremiah.mccurdy@gmail.com>
2022-04-14 19:16:12 +0200

    update app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 import { FC } from 'react';
+import { Provider } from 'react-redux';
 import { ThemeProvider } from '@mui/material/styles';
+import { purple } from '@mui/material/colors';
+import { Box } from '@mui/material';
+
 import { theme } from 'Theme';
+import { store } from 'state/store';
+import { TaskList } from '+TaskList';
+import { CenterCard } from 'components';
 
 export const App: FC = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <div className="App">
-        <header>
-          <p>Hello Vite + React!</p>
-        </header>
-      </div>
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <Box sx={{ padding: '24px 16px', backgroundColor: purple[50], minHeight: 'calc(100vh - 24px)' }}>
+          <CenterCard>
+            <TaskList />
+          </CenterCard>
+        </Box>
+      </ThemeProvider>
+    </Provider>
   );
 };
