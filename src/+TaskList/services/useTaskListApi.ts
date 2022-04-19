221b4673761bc23972debabad629e9553148e53a
Jeremiah McCurdy <jeremiah.mccurdy@gmail.com>
2022-04-19 10:59:35 +0200

    update use api endpoints

--- src/+TaskList/services/useTaskListApi.ts
+++ src/+TaskList/services/useTaskListApi.ts
@@ -4,14 +4,15 @@ import { makeUseAxios } from 'axios-hooks';
 import { Task } from '+TaskList/types';
 
 export const useTaskListApi = makeUseAxios({
-  axios: axios.create({ baseURL: 'http://localhost:3001/' }),
+  axios: axios.create({ baseURL: 'http://localhost:3001/tasklist/api/' }),
 });
 
 export const taskListApi = {
   taskAction() {
-    const apiUrl = 'http://localhost:3001';
+    const apiUrl = 'http://localhost:3001/tasklist/api';
 
     return {
+      getAllTasks: () => axios.get(`${apiUrl}/tasks/`),
       create: (body: Task) => axios.post(`${apiUrl}/tasks`, body),
       update: (taskId: string, body: Task) => axios.put(`${apiUrl}/tasks/${taskId}`, body),
       delete: (taskId: string) => axios.delete(`${apiUrl}/tasks/${taskId}`),
