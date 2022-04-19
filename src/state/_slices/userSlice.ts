e3e822eea6f8d7541906af51806790a9d4816cea
Jeremiah McCurdy <jeremiah.mccurdy@gmail.com>
2022-04-19 11:00:32 +0200

    update user slice error handling

--- src/state/_slices/userSlice.ts
+++ src/state/_slices/userSlice.ts
@@ -1,6 +1,6 @@
 import { createSlice, PayloadAction, createAsyncThunk } from '@reduxjs/toolkit';
 import { RootState } from 'state/store';
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 
 interface UserSlice {
   id: string;
@@ -14,13 +14,18 @@ const initialState: UserSlice = {
   image_url: '',
 };
 
-export const getUserData = createAsyncThunk('user/getUserData', async () => {
-  try {
-    const response = await axios.get('http://localhost:3001/user');
-    return response.data;
-  } catch (err) {
-    throw Error('There was an error retrieving the user');
-  }
+export const getUserData = createAsyncThunk('user/getUserData', async (_, { rejectWithValue }) => {
+  const response = await axios.get('http://localhost:3001/tasklist/api/user');
+  return response.data;
+  // try {
+  //   const response = await axios.get('http://localhost:3001/tasklist/api/user');
+  //   return response.data;
+  // } catch (err: unknown) {
+  //   if (!(err as AxiosError).response) {
+  //     throw new Error('There was an error retrieving the user');
+  //   }
+  //   return rejectWithValue((err as AxiosError).response?.data);
+  // }
 });
 
 export const userSlice = createSlice({
